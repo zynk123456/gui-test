181c4233a938788f47acafe5a63d6127ed003f8d
zynk123456 <kaydequinlan@gmail.com>
2025-01-01 14:38:01 +1100

    Create instant-respawn.lua

---
+++ instant-respawn.lua
@@ -0,0 +1,7 @@
+game:GetService("Players").PlayerAdded:Connect(function(player)
+    player.CharacterAdded:Connect(function(character)
+        character:WaitForChild("Humanoid").Died:Connect(function()
+            player:LoadCharacter()
+        end)
+    end)
+end)
