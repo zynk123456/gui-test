6bdc3aedd871a148e90c7a47caff2847a9162cd7
zynk123456 <kaydequinlan@gmail.com>
2025-01-01 14:35:34 +1100

    Create customcursor.lua

---
+++ customcursor.lua
@@ -0,0 +1,4 @@
+local imageId = 14872113711 -- USE THE IMAGE ID, NOT THE DECAL ID
+
+task.wait(0.25)
+game:GetService("Players").LocalPlayer:GetMouse().Icon = "http://www.roblox.com/asset?id=" .. imageId
